228a55087bc9fa36b5d48a1f9c68a9a325bbae34
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-06-09 23:15:42 +0300

    add clock functions

--- clock/script.js
+++ clock/script.js
@@ -1,1 +1,22 @@
-const test = () => {};
+let currentDate;
+
+const hourNeedle = document.querySelector(".hour");
+const minuteNeedle = document.querySelector(".minute");
+const secondNeedle = document.querySelector(".second");
+
+const updateClock = () => {
+  currentDate = new Date();
+  hourNeedle.style.transform = `translate(-50%, -100%) rotate(${
+    currentDate.getHours() / 12
+  }turn)`;
+
+  minuteNeedle.style.transform = `translate(-50%, -100%) rotate(${
+    currentDate.getMinutes() / 60
+  }turn)`;
+
+  secondNeedle.style.transform = `translate(-50%, -100%) rotate(${
+    currentDate.getSeconds() / 60
+  }turn)`;
+};
+
+setInterval(updateClock, 1000);
