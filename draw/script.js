fcd19fed57868b270c589973d2969a1c891810ee
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-06-18 16:38:42 +0300

    add draw functions

--- draw/script.js
+++ draw/script.js
@@ -1,10 +1,36 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-const SIZE = 20;
+const SIZE = 5;
 const COLOR = "black";
-let X;
-let Y;
+let x;
+let y;
+
+let isPressed = false;
+
+canvas.addEventListener("mousedown", (e) => {
+  isPressed = true;
+  x = e.offsetX;
+  y = e.offsetY;
+});
+
+canvas.addEventListener("mouseup", (e) => {
+  isPressed = false;
+  x = undefined;
+  y = undefined;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (isPressed) {
+    x2 = e.offsetX;
+    y2 = e.offsetY;
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+  }
+
+  x = x2;
+  y = y2;
+});
 
 function drawCircle(x, y) {
   ctx.beginPath();
@@ -18,7 +44,7 @@ function drawLine(x1, y1, x2, y2) {
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
   ctx.strokeStyle = COLOR;
-  ctx.lineWidth = SIZE;
+  ctx.lineWidth = SIZE * 2;
   ctx.stroke();
 }
 
