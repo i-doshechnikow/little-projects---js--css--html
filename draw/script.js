dc6b10b12e902591e13abe5fe4ed4e2370322985
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-06-19 12:33:47 +0300

    add function to change color

--- draw/script.js
+++ draw/script.js
@@ -3,9 +3,11 @@ const ctx = canvas.getContext("2d");
 const sizeBlock = document.querySelector("#size");
 const increaseBtn = document.querySelector(".increase");
 const decreaseBtn = document.querySelector(".decrease");
+const colorPalette = document.querySelector("#color");
+const btnClear = document.querySelector(".clear");
 
-let SIZE = 5;
-const COLOR = "black";
+let size = 5;
+let color = "#000000";
 let x;
 let y;
 
@@ -34,24 +36,28 @@ canvas.addEventListener("mousemove", (e) => {
   }
 });
 
+canvas.addEventListener("mouseleave", (e) => {
+  isPressed = false;
+});
+
 increaseBtn.addEventListener("click", (e) => {
-  if (SIZE < 100) {
-    SIZE += 1;
-    sizeBlock.innerText = SIZE;
+  if (size < 100) {
+    size += 1;
+    sizeBlock.innerText = size;
   }
 });
 
 decreaseBtn.addEventListener("click", (e) => {
-  if (SIZE > 1) {
-    SIZE -= 1;
-    sizeBlock.innerText = SIZE;
+  if (size > 1) {
+    size -= 1;
+    sizeBlock.innerText = size;
   }
 });
 
 function drawCircle(x, y) {
   ctx.beginPath();
-  ctx.arc(x, y, SIZE, 0, Math.PI * 2, true);
-  ctx.fillStyle = COLOR;
+  ctx.arc(x, y, size, 0, Math.PI * 2, true);
+  ctx.fillStyle = color;
   ctx.fill();
 }
 
@@ -59,7 +65,13 @@ function drawLine(x1, y1, x2, y2) {
   ctx.beginPath();
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
-  ctx.strokeStyle = COLOR;
-  ctx.lineWidth = SIZE * 2;
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size * 2;
   ctx.stroke();
 }
+
+colorPalette.addEventListener("change", ({ target }) => (color = target.value));
+
+btnClear.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
